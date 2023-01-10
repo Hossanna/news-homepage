8018359afab08bab0e9c32f69a36ca915a31caef
Hosanna Bivan <hosabivan@gmail.com>
2023-01-11 00:17:06 +0100

    responsiveness

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var navMenu = document.getElementById("navMenu")
+var contact = document.getElementById("contact")
+
+function showMenu(){
+    navMenu.style.right = "0px"
+}
+
+function hideMenu(){
+    navMenu.style.right= "-200px"
+}
+
