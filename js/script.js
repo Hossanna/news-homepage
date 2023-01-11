d995296c308d47d90615c8dffd2bb4279b7a9224
Hosanna Bivan <hosabivan@gmail.com>
2023-01-12 00:11:32 +0100

    responsiveness

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ var navMenu = document.getElementById("navMenu")
 var contact = document.getElementById("contact")
 
 function showMenu(){
-    navMenu.style.right = "0px"
+    navMenu.style.display = "block"
 }
 
 function hideMenu(){
-    navMenu.style.right= "-200px"
+    navMenu.style.display= "none"
 }
 
