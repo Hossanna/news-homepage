d34e8cdda67d54cecff067ce47149f6bd4935209
Hosanna Bivan <hosabivan@gmail.com>
2023-01-14 10:04:21 +0100

    hamburger nav

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
 
 
 var navMenu = document.getElementById("navMenu")
-var contact = document.getElementById("contact")
+var logo = document.getElementById("logo")
 
 function showMenu(){
-    navMenu.style.right = "0px"
+    
+        navMenu.className = "navbar"
+        logo.style.marginRight = "50%"
+    
+
+    // navMenu.classList.remove("navbar")
+    // navMenu.classList.add("nonee")
 }
 
 function hideMenu(){
-    navMenu.style.right= "-200px"
+    
+        navMenu.className = "nonee"
+        logo.style.marginRight = "70%"
+
+
+    // navMenu.classList.remove("nonee")
+    // navMenu.classList.add("navbar")
 }
+
+// function showMenu(){
+//     navMenu.style.right = "0px"
+// }
+
+// function hideMenu(){
+//     navMenu.style.right= "-200px"
+// }
 
 // function showMenu(){
 //     navMenu.style.display = "block"
